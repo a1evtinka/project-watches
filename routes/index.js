3dd681fd16b9e033ff72da18eb245fcf1e6ad7a4
Pavel Egorov <egorovpvspb@gmail.com>
2022-07-07 19:54:06 +0300

    удалил код для рассылки почты он используется в другом файле

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,6 @@ const Form = require('../views/Form');
 //   res.end(html);
 // });
 
-// const router = require('express').Router();
-// const nodemailer = require('nodemailer');
-// const transporter = require('./nodemailer');
-const indexRouter = require('express').Router();
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
 // const { Watch } = require('../db/models');
 // const WatchesList = require('../views/WatchesList');
 
@@ -29,33 +23,14 @@ const ReactDOMServer = require('react-dom/server');
 //     res.status('500').send('Database failure');
 //     return;
 //   }
-//
+
 //   const watcheslist = React.createElement(WatchesList, {
 //     watch,
-//     user: { name: 'AAAAAA' }, //res.locals.user,
+//     user: { name: 'AAAAAA' }, // res.locals.user,
 //   });
 //   const html = ReactDOMServer.renderToStaticMarkup(watcheslist);
 //   res.write('<!doctype html>');
 //   res.end(html);
-// });
-// const mail = {
-//   from: 'Yashwant Chavan <a1evtina@list.ru>',
-//   to: 'a1evtina@list.ru',
-//   subject: 'Send Email Using Node.js',
-//   text: 'Node.js New world for me',
-//   html: '<b>Node.js New world for me</b>',
-// };
-
-// router.get('/', (req, res) => {
-//   transporter.sendMail(mail, (error, response) => {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log(`Message sent: ${response.message}`);
-//     }
-
-//     transporter.close();
-//   });
 // });
 
 module.exports = indexRouter;
