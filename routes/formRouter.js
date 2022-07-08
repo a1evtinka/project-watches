250872b1872d5e9b861a59448548c06b717a0d1d
Alevtina <a1evtina@list.ru>
2022-07-08 14:38:10 +0300

    orderMail

--- routes/formRouter.js
+++ routes/formRouter.js
@@ -1,10 +1,11 @@
 // const ReactDOMServer = require('react-dom/server');
 // const React = require('react');
 
-const multer = require('multer');
-const upload = multer({ dest: '../public/uploads' });
+// const multer = require('multer');
+// const upload = multer({ dest: '../public/uploads' });
 const formRouter = require('express').Router();
 const Form = require('../views/Form');
+const transporter = require('./nodemailer');
 
 // console.log('!!!!!!!!', Form);
 const {
@@ -38,6 +39,28 @@ formRouter.post('/', async (req, res) => {
     phone,
     watch_id,
   });
+
+  // отправка письма start
+  // console.log('мыло пользователя', email);
+
+  const mail = {
+    from: 'Pavel Egorov <pewatches2022@gmail.com>',
+    to: email,
+    subject: `Dear ${name}! Thank you for your order!`,
+    text: `Dear ${name}!`,
+    html: '<b>Thank you for your interest! We will call you in 5 min. Pavel Egorov Watches</b>',
+  };
+
+  transporter.sendMail(mail, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(`Message sent: ${response.message}`);
+    }
+
+    transporter.close();
+  });
+  // отправка письма finish
 
   res.json({
     status: 'ок',
